4f3cbb1fb1c691f0870b529cdd516d4585f822fb
KARTHIK K <74089000+k-karthik7@users.noreply.github.com>
2024-09-14 13:40:44 +0530

    preview img retrive check

--- public/blog-previews.js
+++ public/blog-previews.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const previewDiv = document.createElement('div');
             previewDiv.className = 'blog-preview';
             previewDiv.innerHTML = `
-                <img src="articles/${preview.id}/${preview.image}" alt="${preview.title}">
+                <img src="https://blog.karthikkotha.in/articles/${preview.id}/${preview.image}" alt="${preview.title}">
                 <div class="preview-story">
                 <div class="blog-title">
                 <h4><a href="https://blog.karthikkotha.in/articles/${preview.id}/${preview.article}" target="_blank">${preview.title}</a></h4>
